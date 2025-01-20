5d02e302ea255953ef7306f9628736ad17856808
Mickaeldolle <dolle.mickael@gmail.com>
2025-01-20 17:50:45 +0100

    :bug: update import mobileNavBar in profile layout

--- app/shop/layout.tsx
+++ app/shop/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import PreviousIcon from "../components/ui/PreviousIcon";
-import { Suspense } from "react";
 import MobileNavBar from "../components/MobileNavBar/MobileNavBar";
-// import { CustomerStoreProvider } from '@/providers/customer-store-provider'
 
 
 export const metadata: Metadata = {
@@ -19,11 +16,9 @@ export default function RootLayout({
         <div className="min-h-dvh">
             <MobileNavBar />
             <div className="bg-background p-2 flex flex-col ">
-                {/* <CustomerStoreProvider > */}
                 <main className="flex-1">
                     {children}
                 </main>
-                {/* </CustomerStoreProvider> */}
             </div>
         </div>
 
